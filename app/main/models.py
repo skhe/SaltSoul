0d845902eb2f9432ceab12c154a17b7a2c307249
skhe <qchb521@live.com>
2018-08-24 21:46:22 +0800

    update models

--- app/main/models.py
+++ app/main/models.py
@@ -39,18 +39,58 @@ class Religion(db.Document):
     """
     宗教
     """
-    pass
+    name = db.StringField(required=True)        # 名称
+    token = db.StringField(required=True)       # 信物
+    tasks = db.ListField(db.DictField())        # 任务
+    rewards = db.ListField(db.DictField())      # 奖励
+    remarks = db.ListField(db.StringField())    # 备注
+
+    def __unicode__(self):
+        return "Religion {}".format(self.name)
 
 
 class Weapon(db.Document):
     """
     武器
     """
-    pass
+    WEAPON_TYPES = ('匕首', '剑', '锤', '斧', '鞭', '戟', '枪', '镰',
+                    '巨剑', '大锤', '巨斧', '弓', '弩', '铳', '魔杖', '法杖')
 
+    name = db.StringField(required=True)                                # 名称
+    weapon_type = db.StringField(required=True, choices=WEAPON_TYPES)   # 类型
+    symbol = db.StringField()                                           # 图标
+    grade_required = db.StringField()                                   # 专精等级
+    weight = db.FloatField()                                            # 重量
+    attack_power = db.StringField()                                     # 伤害
+    special_effect = db.StringField()                                   # 特殊效果
+
+    def __unicode__(self):
+        return "Weapon {}".format(self.name)
+ß
 
 class Boss(db.Document):
     """
     Boss
     """
-    pass
+    name = db.StringField(required=True)        # 名称
+    hp = db.IntField()                          # 血量
+    position = db.StringField(required=True)    # 位置
+    defences = db.DictField()                   # 抗性
+    rewards = db.ListField(db.StringField())    # 掉落
+
+    def __unicode__(self):
+        return "Boss {}".format(self.name)
+
+
+class ChapterBoss(db.Document):
+    """
+    ChapterBoss
+    """
+    name = db.StringField(required=True)                                    # 名称
+    image = db.StringField()                                                # 图片
+    remark = db.StringField()                                               # 备注
+    bosses = db.LisiField(db.ReferenceField('Boss',
+                                            reverse_delete_rule=db.DENY))   # Boss
+
+    def __unicode__(self):
+        return "ChapterBoss {}".format(self.name)
